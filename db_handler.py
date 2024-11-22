afc7bf62142d31382169f1fedc4cf1e7b916e470
Mohammed-AhmedAF <mohamedstarwars@gmail.com>
2024-11-22 20:43:08 +0200

    First commit

---
+++ db_handler.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect("customer.db")
+
+cur = conn.cursor()
+
+# Create a table
+def createDB(cur):
+    cur.execute("""CREATE TABLE customers (
+            first_name TEXT,
+            last_name TEXT,
+            email TEXT
+            )""")
+    conn.commit()
+
+
+def addOneCustomer():
+    cur.execute("INSERT INTO customers VALUES ('John','Elder','john@codemy.com')")  
+    conn.commit()
+
+addOneCustomer()
+cur.execute("SELECT * FROM customers")
+print(cur.fetchall()[0])
+print(cur.fetchall()[1])
+
+
+conn.close()
